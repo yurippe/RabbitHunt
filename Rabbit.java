3930db038a9370655d0bbe02936b6de1f91983d1
yurippe <yurippenet@gmail.com>
2015-09-25 17:38:45 +0200

    Implemented task 2

--- Rabbit.java
+++ Rabbit.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class Rabbit here.
  * 
@@ -25,17 +25,56 @@ public class Rabbit extends Animal {
      */
     @Override
     public Direction decideDirection(){
-    
-        
         return Direction.STAY;
     }
+    
+    
+    /**
+     * Moves out of the foxes field of view
+     * (Oppgave 1)
+     */
+    public Direction decideDirection1(){
+        
+       Direction foxDir = Direction.STAY;
+       for(Direction d : Direction.allDirections()){
+        Class<?> type = look(d);
+        if(type == Fox.class){
+            foxDir = d;
+            break;
+        }
+       }
+       switch (foxDir){
+            case N:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.E, Direction.W, Direction.SW, Direction.SE)));
+            case NE:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.S, Direction.W, Direction.NW, Direction.SE)));
+            case E:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.N, Direction.S, Direction.NW, Direction.SW)));
+            case SE:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.N, Direction.W, Direction.NE, Direction.SW)));
+            case S:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.E, Direction.W, Direction.NW, Direction.NE)));
+            case SW:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.N, Direction.E, Direction.SE, Direction.NW)));
+            case W:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.N, Direction.S, Direction.NE, Direction.SE)));
+            case NW:
+                return getLongestFreeDirection(new ArrayList<Direction>(Arrays.asList(Direction.NE, Direction.E, Direction.S, Direction.SW)));
+            default:
+                return Direction.STAY;
+       }
+    
+    }
+    
+    
         
     /** 
      * Waits for the fox, then runs in circles
      * NOT effective versus more than one fox
-     * 
+     * (Oppgave 2)
      */
         public Direction decideDirection2(){
+            //Initialize variables;
             Class<?> c;
             
             for(Direction d : Direction.allDirections()){
@@ -76,7 +115,28 @@ public class Rabbit extends Animal {
         }
     }
     
-    
+    /**
+     * Gets the direction with most free spaces.
+     *  
+     */
+    private Direction getLongestFreeDirection(ArrayList<Direction> directions){
+        int bestLength = 0;
+        Direction bestDir = Direction.STAY;
+        for(Direction d : directions){
+            Class<?> type = look(d);
+            if(type == Fox.class){
+                continue;
+            }
+            
+            if(bestLength < distance(d)) {
+                bestLength = distance(d);
+                bestDir = d;
+            }
+        }
+        
+        return bestDir;
+        
+    }
     
     /**
      * This method is used to retrieve who the authors are.
